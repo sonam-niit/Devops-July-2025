a19b4724c3592dd05239d64538a7d5a99a40a0ad
sonam-niit <sonam.gravity@gmail.com>
2025-08-17 18:57:07 +0530

    updated files

--- Session-9-bash-cloud/special-var.sh
+++ Session-9-bash-cloud/special-var.sh
@@ -6,3 +6,11 @@ echo "Second argument: $2"
 echo "All arguments: $@"
 echo "Process Id: $$"
 echo "Exit Status: $?"
+
+# Run above script using below command
+# sh special-var.sh 67 sonam true hello xyz
+# here (67 sonam true hello xyz) are the arguments  
+
+#./special-var.sh file1.txt file2.txt
+# here 1st arg: file1.txt
+# here 2nd arg: file2.txt
--- Session-9-bash-cloud/test.sh
+++ Session-9-bash-cloud/test.sh
@@ -5,3 +5,7 @@ mkdir -p sonam
 cd sonam
 echo "this is my new file">file.txt
 echo "File Created successfully"
+
+# to run this open git bash or wsl
+# just give file name to run ./test.sh
+# or you can give sh test.sh command to run the script
